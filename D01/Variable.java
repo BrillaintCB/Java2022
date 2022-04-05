0ab4d4ce7451379f4e0f19bd44a3f04da31d35a4
Choongbae Park <J378843@tafe.wa.edu.au>
2022-04-05 11:34:43 +0800

    Varaible add

---
+++ D01/Variable.java
@@ -0,0 +1,20 @@
+package D01;
+
+public class Variable {
+    
+    public static void main(String[] args) {
+        
+    boolean employeed = false;
+    String name = "CB";
+    char gender = 'M';
+    int year = 2022;
+    double salary = 3620.5;
+
+
+
+
+    System.out.printf("%s : Employed : %b Gender %c, %d , Salary %f",name, employeed, gender, year, salary);
+    }
+
+
+}
