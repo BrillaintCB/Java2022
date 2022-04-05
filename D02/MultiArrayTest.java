77f7dec9f5b8da1112bf6402ab341aeabfa70fba
Choongbae Park <J378843@tafe.wa.edu.au>
2022-04-05 18:46:46 +0800

    MultiArrayTest

---
+++ D02/MultiArrayTest.java
@@ -0,0 +1,19 @@
+package D02;
+
+public class MultiArrayTest {
+    public static void main(String[] args) {
+        int[] arr1 = new int[3];
+
+        int[][] arr2;
+
+        arr2 = new int[2][3];
+
+
+        System.out.println("arr2배열의 행의 수 : " + arr2.length + "\n"); 
+        System.out.println("arr2배열의 2행의 열의 수 : " + arr2[1].length + "\n"); 
+
+        
+
+
+    }
+}
